328db77cf3eec35266c8649371836f238ae9eb85
umer khan <umer.kdev@gmail.com>
2018-10-09 21:08:59 +0300

    adding custom checkbox

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,8 +9,14 @@
 
         <ul>
 
-            <li v-for="(task, index) in Tasks" :key="index">
-                <input type="checkbox" @change="changeCheck(task)" :checked="task.isComplete" />{{task.Title}}
+            <li v-for="(task, index) in Tasks" :key="index" :class="{completed : task.isComplete}">
+
+
+
+                <label class="container">{{task.Title}}
+                    <input  type="checkbox" @change="changeCheck(task)" :checked="task.isComplete" />
+                    <span class="checkmark"></span>
+                </label>
             </li>
 
         </ul>
@@ -47,6 +53,7 @@
             ]),
             addTask(){
                 this.ADD_TASK({Title : this.newTask, isComplete : false})
+                this.newTask = ''
             }
         }
 
@@ -78,15 +85,13 @@
             width: 100%;
             display: flex;
 
-            input {
-               width: 1rem;
-            }
         }
 
         input {
             padding: 12px;
             width: 100%;
             border: 1px solid $border-color;
+            font-size: 22px;
         }
 
         hr {
@@ -101,6 +106,75 @@
 
         .completed {
             border: 1px solid aquamarine;
+        }
+
+
+        //custom checkbox css
+        .container {
+            display: block;
+            position: relative;
+            padding-left: 35px;
+            //margin-bottom: 12px;
+            cursor: pointer;
+            font-size: 22px;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+
+        /* Hide the browser's default checkbox */
+        .container input {
+            position: absolute;
+            opacity: 0;
+            cursor: pointer;
+            height: 0;
+            width: 0;
+        }
+
+        /* Create a custom checkbox */
+        .checkmark {
+            position: absolute;
+            top: 3px;
+            left: 0;
+            height: 25px;
+            width: 25px;
+            background-color: #eee;
+        }
+
+        /* On mouse-over, add a grey background color */
+        .container:hover input ~ .checkmark {
+            background-color: #ccc;
+        }
+
+        /* When the checkbox is checked, add a blue background */
+        .container input:checked ~ .checkmark {
+            background-color: #2196F3;
+        }
+
+        /* Create the checkmark/indicator (hidden when not checked) */
+        .checkmark:after {
+            content: "";
+            position: absolute;
+            display: none;
+        }
+
+        /* Show the checkmark when checked */
+        .container input:checked ~ .checkmark:after {
+            display: block;
+        }
+
+        /* Style the checkmark/indicator */
+        .container .checkmark:after {
+            left: 9px;
+            top: 5px;
+            width: 5px;
+            height: 10px;
+            border: solid white;
+            border-width: 0 3px 3px 0;
+            -webkit-transform: rotate(45deg);
+            -ms-transform: rotate(45deg);
+            transform: rotate(45deg);
         }
 
     }
